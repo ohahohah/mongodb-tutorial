5520d8088dbb61680213dcafea5d31200ad602ee
SiYoungOh <ohahohah.dev@gmail.com>
2021-08-28 02:36:06 +0900

    Add script type

--- setting.sh
+++ setting.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # Import the public key used by the package management system
 sudo apt-get install gnupg
 wget -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | sudo apt-key add -
