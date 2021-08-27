26254285a2653dda440b0b6d4cfdd1583d786c0b
SiYoungOh <ohahohah.dev@gmail.com>
2021-08-28 02:10:03 +0900

    Add command when error occur, stopped

--- setting.sh
+++ setting.sh
@@ -1,3 +1,5 @@
+set -e
+
 # Import the public key used by the package management system
 sudo apt-get install gnupg
 wget -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | sudo apt-key add -
