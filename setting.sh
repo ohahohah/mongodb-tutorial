3de272a40a83b5174f3bc083a4e1ff994f37d091
SiYoungOh <ohahohah.dev@gmail.com>
2021-08-28 01:00:21 +0900

    Create setting.sh

---
+++ setting.sh
@@ -0,0 +1,34 @@
+# Import the public key used by the package management system
+sudo apt-get install gnupg
+wget -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | sudo apt-key add -
+
+# Install Mongodb
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/5.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-5.0.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org=5.0.2 mongodb-org-database=5.0.2 mongodb-org-server=5.0.2 mongodb-org-shell=5.0.2 mongodb-org-mongos=5.0.2 mongodb-org-tools=5.0.2
+sudo mkdir -p /data/db
+
+# get mongodb config file
+wget -q0 
+
+# Run Mongodb
+sudo service mongod start
+sleep 15
+netstat -tnlp
+
+# setting - create user
+mongo admin --eval 'db.createUser({user: "myuser", pwd: "mypassword", roles:["root"]});'
+
+# replace config - bindip,security 
+cat A > /etc/mongod.conf
+
+# restart mongodb
+sudo service mongod stop
+sudo service mongod start
+sleep 15
+netstat -tnlp
+
+
+
+ 
+
